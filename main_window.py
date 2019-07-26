35b70b61e8e4564c93ceccb68c7464c8ce5c26b6
Demetrius Veras De Oliveira <demetrius.v@samsung.com>
2019-07-26 10:47:20 -0400

    load csv file function created

--- main_window.py
+++ main_window.py
@@ -1,7 +1,8 @@
 import sys
+import pandas as pd
 
 from PySide2 import QtWidgets
-from PySide2.QtCore import QFile, Qt
+from PySide2.QtCore import QFile, Qt, Slot
 from PySide2.QtUiTools import QUiLoader
 
 
@@ -9,16 +10,38 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def __init__(self):
         super(MainWindow, self).__init__()
-        self.window = None
-        self.setup_ui()
 
-    def setup_ui(self):
         loader = QUiLoader()
         file = QFile('layouts/mainwindow.ui')
         file.open(QFile.ReadOnly)
         self.window = loader.load(file, self)
         file.close()
+        self.window.setWindowTitle('Plot BI')
         self.window.show()
+
+        self.setup_slots()
+
+        # attributes
+        self._data = None
+
+    def setup_slots(self):
+        self.window.actionOpen.triggered.connect(self.load_csv_file)
+        self.window.actionQuit.triggered.connect(self.exit_application)
+
+    @Slot()
+    def exit_application(self):
+        QtWidgets.QApplication.exit()
+
+    @Slot()
+    def load_csv_file(self):
+        file_path = QtWidgets.QFileDialog.getOpenFileName(self, 'Open CSV file', '', 'CSV (*.csv);;All file (*)')
+        if file_path:
+            self._data = pd.read_csv(file_path[0], sep=';', encoding='latin1')
+            heads = [i for i in self._data.head(0)]
+            self._popupate_fields(heads)
+
+    def _popupate_fields(self, heads):
+        self.window.cb_heads.addItems(heads)
 
 
 if __name__ == "__main__":
